8b2013ad8b16bd394a20190ae2e5aa4161726cb7
PieroAvellanedaLinares <142689838+PieroAvellanedaLinares@users.noreply.github.com>
2023-10-07 14:07:24 -0500

    Add files via upload

---
+++ src/Alumno.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author piero
+ */
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private String estado;
+
+    public Alumno(String nombre, String apellido, String estado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
